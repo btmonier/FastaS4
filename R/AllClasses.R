e752708a9417eb24e522057f1fb0f9ac4f5b663e
Brandon Monier <brandon.monier@gmail.com>
2018-12-23 23:08:44 -0500

    Add read functions

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,5 @@
+# === Classes for FastaS4 ===========================================
+
 #' @title Class FASTA
 #' 
 #' @description Class \code{FASTA} defines a FASTA file
@@ -8,7 +10,29 @@
 setClass(
     Class = "FASTA",
     representation = representation(
-        seqID = "list",
-        seq = "list"
+        seqID = "character",
+        seq = "character"
     )
 )
+
+
+#' @title Wrapper function for FASTA class
+#'
+#' @description This function provides a wrapper to create \code{FASTA} class
+#'    objects based on a path to a FASTA file.
+#' 
+#' @param path a path to a FASTA file
+#' 
+#' @name FASTA
+#' @rdname FASTA-class
+#' 
+#' @importFrom methods new
+#' @export
+FASTA <- function(path) {
+    x <- readFasta(path)
+    new(
+        Class = "FASTA", 
+        seqID = x[["seqID"]],
+        seq = x[["seq"]]
+    )
+}
--- R/ReadFunctions.R
+++ R/ReadFunctions.R
@@ -1,9 +1,95 @@
+# === Read functions for FastaS4 ====================================
+
+## readFASTA - not exported
 readFasta <- function(path) {
-    x <- scan(path, character(), quote = "", quiet = TRUE)
-    x[seq_along(x) %% 2 > 0] <- gsub(
-        pattern = ">",
-        replacement = "",
-        x = x[seq_along(x) %% 2 > 0]
+    fas <- scan(path, character(), quote = "", quiet = TRUE, sep = "\n")
+    intervals <- grep("^>", fas, value = FALSE)
+    fas[intervals] <- paste("@@@", fas[intervals], "@@@", sep = "")
+    fas <- unlist(strsplit(paste(fas, collapse = ""), "@@@"))
+    seqnames <- grep("^>", fas, value = TRUE)
+    loh <- paste(c("^[", LETTERS, letters, "-]"), collapse = '')
+    seqs <- grep(loh, fas, value = TRUE)
+    ids <- fas[grep("^>", fas, value = FALSE)]
+    ids <- gsub(">" , "", ids)
+    return(
+        list(
+            seqID = ids,
+            seq = seqs
+        )
     )
-    return(x)
+}
+
+
+## Summarizer - not exported
+summFASTA <- function(object) {
+    xID <- object@seqID
+    xID <- sprintf(paste0("%0-", max(nchar(xID)), "s"), xID)
+    xNO <- sprintf(paste0("%0", max(nchar(seq_along(xID))), "s"), seq_along(xID))
+    xSQL <- nchar(object@seq)
+    
+    xSQ <- object@seq
+    xSQ <- ifelse(
+        test = nchar(xSQ) > 10, 
+        yes = paste(
+            substr(xSQ, start = 1, stop = 5), 
+            "...", 
+            substr(xSQ, start = nchar(xSQ) - 5, stop = nchar(xSQ))
+        ), 
+        no = paste(xSQ)
+    )
+    xSQ <- sprintf(paste0("%0-", max(nchar(xSQ)), "s"), xSQ)
+    
+    xHD <- c(
+        sprintf(paste0("%0-", max(nchar(seq_along(xID))), "s"), ""),
+        sprintf(paste0("%0-", max(nchar(xID)), "s"), "ID"),
+        sprintf(paste0("%0-", max(nchar(xSQ)), "s"), "Sequence"),
+        sprintf(paste0("%0-", max(nchar(nchar(object@seq))), "s"), "Length")
+    )
+    
+    xSP <- c(
+        sprintf(paste0("%0-", max(nchar(seq_along(xID))), "s"), ""),
+        sprintf(paste0("%0-", max(nchar(xID)), "s"), "- - -"),
+        sprintf(paste0("%0-", max(nchar(xSQ)), "s"), "- - -"),
+        sprintf(paste0("%0-", max(nchar(nchar(object@seq))), "s"), "- - -")
+    )
+    
+    
+    
+    if (length(xID) <= length(utils::head(xID)) + length(utils::head(xID))) {
+        cat("A FASTA class with", length(object@seq), "IDs\n")
+        cat(xHD[1], " ", xHD[2], " ", xHD[3], " ", xHD[4], "\n")
+        cat(
+            paste0(
+                "[", xNO, "] ", 
+                xID, " | ", 
+                xSQ, " | ", 
+                nchar(object@seq)
+            ),
+            sep = "\n"
+        )
+    } else {
+        cat("A FASTA class with", length(object@seq), "IDs\n")
+        cat("Showing the first and last 5 IDs...\n")
+        cat(xHD[1], " ", xHD[2], " ", xHD[3], " ", xHD[4], "\n")
+        cat(
+            paste0(
+                "[", utils::head(xNO, 5), "] ", 
+                utils::head(xID, 5), " | ", 
+                utils::head(xSQ, 5), " | ", 
+                nchar(utils::head(object@seq, 5))
+            ),
+            sep = "\n"
+        )
+        cat(xSP[1], " ", xSP[2], " ", xSP[3], " ", xSP[4], "\n")
+        cat(
+            paste0(
+                "[", utils::tail(xNO, 5), "] ", 
+                utils::tail(xID, 5), " | ", 
+                utils::tail(xSQ, 5), " | ", 
+                nchar(utils::tail(object@seq, 5))
+            ),
+            sep = "\n"
+        )
+    }
+    
 }
